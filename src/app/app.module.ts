9980d22100723f78455ce7042d85bc47226bd285
admin <admin@admin.com>
2021-11-09 15:43:07 +0600

    Created a component for the playing field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { FieldComponent } from './game/field/field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    FieldComponent
   ],
   imports: [
     BrowserModule
